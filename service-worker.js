2d49571d3e07ab9c2cdc47a25f89f5755e59c4b4
nakeusa12 <snakeusa8@gmail.com>
2020-05-29 20:39:17 +0700

    membuat website tetap berjalan saat offline

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "winplay-v1";
+var CACHE_NAME = "winplay-v2";
 var urlsToCache = [
   "/",
   "/nav.html",
@@ -7,18 +7,18 @@ var urlsToCache = [
   "/pages/ranks.html",
   "/pages/teams.html",
   "/pages/favorite.html",
-  "/css/materialize.min.css",
+  "/css/materialize.css",
   "/css/main.css",
   "/js/materialize.min.js",
   "/js/nav.js",
   "/js/api.js",
   "/js/register-sw.js",
-  "/img/logo.png",
-  "/img/icon512px.png",
-  "/img/favicon16x16.png",
-  "/img/arrow.svg",
-  "/img/jumbtron-home.jpg",
   "/manifest.json",
+  "/img/arrow.svg",
+  "/img/favicon16x16.png",
+  "/img/icon512px.png",
+  "/img/logo.png",
+  "/img/jumbtron-home.jpg",
 ];
 
 self.addEventListener("install", function (event) {
@@ -27,6 +27,29 @@ self.addEventListener("install", function (event) {
       return cache.addAll(urlsToCache);
     })
   );
+});
+
+self.addEventListener("fetch", function (event) {
+  var base_url = "https://api.football-data.org/";
+
+  if (event.request.url.indexOf(base_url) > -1) {
+    event.respondWith(
+      caches.open(CACHE_NAME).then(function (cache) {
+        return fetch(event.request).then(function (response) {
+          cache.put(event.request.url, response.clone());
+          return response;
+        });
+      })
+    );
+  } else {
+    event.respondWith(
+      caches
+        .match(event.request, { ignoreSearch: true })
+        .then(function (response) {
+          return response || fetch(event.request);
+        })
+    );
+  }
 });
 
 self.addEventListener("activate", function (event) {
